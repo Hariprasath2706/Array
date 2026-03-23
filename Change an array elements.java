81841a0c9376ea4104045bd2e1e43fcacb5f009d
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-23 13:57:53 +0530

    Create Change an array elements.java

---
+++ Change an array elements.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        String[] names = {"Hari","Prasath","Hello"};
+        names[0] = "Vikram";
+        System.out.print(names[0]);
+    }
+}
